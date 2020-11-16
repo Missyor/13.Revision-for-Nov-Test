3a4b131ba0b580e9d4387c37d372a433034eb735
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-16 11:23:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("hello")
+variable = "Alex"
+personName = input("what is your name?: ")
+print(variable)
+print(personName)
+print("hello", personName)
+####int & float, string - letters
+personName = int(input("what is your age?: "))
+personName = float(input("what is your age?: "))
+list = ["dion", "dan", 1234, "scott", "123 hello"]
+print(list)
+
+import random #random number generator
+choice = random.choice(list) #choose random element from list
+print(choice)
+###4 generations of computing
+###people in comp science
